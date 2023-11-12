47f9707d7c31376ac0b18aab54d448c41c59daa7
pkdvalis <79821659+pkdvalis@users.noreply.github.com>
2023-11-12 09:33:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-4b8mwa
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/pkdvalis/stackblitz-starters-4b8mwa)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  let a = 'stand';
+
+  return (
+    <div>
+      <h1>{a}</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
